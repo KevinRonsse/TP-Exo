c4c7f99434ec0000b4e54a0de1af4ecfb86effe4
Kevin Ronsse <103761732+KevinRonsse@users.noreply.github.com>
2022-05-19 11:56:57 +0200

    docker

---
+++ tp-e-commerce/src/Composant/Article/article.js
@@ -0,0 +1,11 @@
+
+
+
+export function Article(props) {
+    return(
+        <div>
+            
+        </div>
+    )
+
+}
---
+++ tp-e-commerce/src/Container/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./Header.css"
+
+
+
+
+
+
+export function Header(){
+
+    return(
+        <>
+        <h1 >E-commerce app </h1>
+        </>
+    )
+
+
+
+}
